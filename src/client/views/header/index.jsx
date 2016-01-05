cd9119e3cf23360f24e9d3831c8b99bb34a68ae2
Severin Ibarluzea <seveibar@gmail.com>
2016-01-05 14:11:26 -0500

    basic menus

--- src/client/views/header/index.jsx
+++ src/client/views/header/index.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import Menu from 'rc-menu';
+var SubMenu = Menu.SubMenu;
+var MenuItem = Menu.Item;
 
 export default class HeaderView extends React.Component {
     constructor(props) {
@@ -8,7 +11,29 @@ export default class HeaderView extends React.Component {
     }
 
     render() {
+        const topMenu = ( <Menu mode='horizontal' className='top-menu'>
+            <MenuItem>File</MenuItem>
+            <MenuItem>Simulate</MenuItem>
+        </Menu> );
+        const bottomMenu = ( <Menu mode='horizontal' className='bottom-menu'>
+            <MenuItem>New</MenuItem>
+            <MenuItem>Save</MenuItem>
+            <MenuItem>Load</MenuItem>
+        </Menu> );
+
         return <div className="header-bar">
-               </div>;
+            <div>{topMenu}</div>
+            <div>{bottomMenu}</div>
+        </div>;
+        // return <div className="header-bar">
+        //             <TabMenu>
+        //                 <FileMenu disabled=true />
+        //             </TabMenu>
+        //             <ButtonMenu>
+        //                 <ButtonGroup name="Begin">
+        //                     <Button name="Load File"/>
+        //                 </ButtonGroup>
+        //             </ButtonMenu>
+        //        </div>;
     }
 }
