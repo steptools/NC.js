04d485d29803002a924d12ef81ea19d58610a813
Samson <bonfante@steptools.com>
2016-05-05 12:15:06 -0400

    Need an endpoint for getting a specific Workingstep- used by the mobile UI to find out the appropriate text to display in the footer.

--- src/server/api/v1/step.js
+++ src/server/api/v1/step.js
@@ -1,3 +1,4 @@
+"use strict";
 var request = require('request');
 
 // TODO define in configuration file
@@ -8,7 +9,23 @@ function endpoint(name){
   return stepServerURL + ":" + stepServerPort + name;
 }
 
+function _getws(req,res){
+  if(req.params.ncId && req.params.wsId){
+	let ncId = req.params.ncId;
+	let wsId = req.params.wsId;
+	request(endpoint("/projects/"+ncId+"/plan/"+wsId), function(err,reqres,body){
+	if(!err && reqres.statusCode ==200)
+		res.status(200).send(body);
+	
+	else
+		res.status(200).send("{'error':null}");
+	});
+
+  }
+}
+
 module.exports = function(app, cb){
+  app.router.get('/v1/nc/:ncId/plan/:wsId',_getws);
   app.ioServer.on('connection', function(socket){
     console.log("New Socket Connection");
 
