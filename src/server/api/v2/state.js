4043c2e24589878350c56fbb65e63600a396fdef
SjBeale <beales@rpi.edu>
2016-06-10 14:22:20 -0400

    made _getdelta

--- src/server/api/v2/state.js
+++ src/server/api/v2/state.js
@@ -1,25 +1,20 @@
 "use strict";
 var StepNC = require('../../../../../StepNCNode/build/Release/StepNC');
-var machinestate = new StepNC.machineState("model.stpnc");
 
 function _getnext(pid,res) {
-
-  machinestate.LoadMachine(pid);
-  var n_ws = machinestate.nextWS();
-  res.send(n_ws);
+  var machinestate = new StepNC.machineState(pid);
+  machinestate.nextWS();
 }
 
 function _getdelta(pid,key,res) {
-  machinestate.LoadMachine(pid);
+  var machinestate = new StepNC.machineState(pid);
   if(!key){
-    var rtn_delta = machinestate.GetDeltaJSON();
+    let rtn_delta = machinestate.GetDeltaJSON();
     res.send(rtn_delta);
   }
   else{
-    var rtn_key = machinestate.GetKeystateJSON();
+    let rtn_key = machinestate.GetKeystateJSON();
     res.send(rtn_key);
   }
 
 }
-
-console.log(_getdelta(94989 ,false));
