a660c6644e954085ed663972c31ac554eefd38a6
nicholasfay <fayn@rpi.edu>
2016-06-13 14:25:26 -0400

    updated file.js to have a cached version of pathmap.json so it isn't loaded each time someone calls getPath

--- src/server/api/v2/file.js
+++ src/server/api/v2/file.js
@@ -1,11 +1,14 @@
 "use strict";
+
 var fs = require("fs");
 //Query for a json file that maps all projects in the data directory
 //to a particular path in the data folder
 
+var content = fs.readFileSync("data/pathmap.json");
+var jsoncontent = JSON.parse(content);
+
+
 module.exports.getPath = function (ncId){
-	let content = fs.readFileSync("data/pathmap.json");
-	let jsoncontent = JSON.parse(content);
 	let lowncId = ncId.toLowerCase();
 	if(jsoncontent[lowncId])
 		return jsoncontent[lowncId];
