06f6ebc18b8a85e6649ea4476921304e675ef50e
Will Rigby-Hall <will@wgrh.io>
2016-06-22 10:34:45 -0400

    removed user and browser references in routes.js

--- src/client/routes.js
+++ src/client/routes.js
@@ -1,15 +1,10 @@
 /* Copyright G. Hemingway, 2015 - All rights reserved */
 "use strict";
 
-import request from 'superagent';
-
 import React                from 'react';
 import ReactDOM             from 'react-dom';
-import BrowserView          from './views/browser';
-import LoginView            from './views/user/login';
-import RegisterView         from './views/user/register';
-import ResponsiveView      from './views/responsive';
-// import SidebarView           from './views/sidebar';
+import request              from 'superagent';
+import ResponsiveView       from './views/responsive';
 let qs                      = require('qs');
 const queryString =         require('query-string');
 
@@ -18,8 +13,6 @@ const queryString =         require('query-string');
 module.exports = Backbone.Router.extend({
     routes: {
         '':                             '_landing',
-        'login':                        '_login',
-        'register':                     '_register',
         'stepnc/:projectid':            '_stepnc',
         ':modelID':                     '_model',
         '*path':                        '_default',
@@ -32,75 +25,28 @@ module.exports = Backbone.Router.extend({
         console.log('Landing path');
     },
 
-    _browse: function() {
-        if (this.app.config.auth && !this.app.state.user) {
-            // Redirect to Login if auth required and not done
-            this.navigate('login', { trigger: true });
-        } else {
-            ReactDOM.render(<BrowserView
-                router={this}
-                user={this.app.state.user}
-                token={this.app.state.token}
-                config={this.app.config}
-                services={this.app.services}
-                socket={this.app.socket}
-            />, document.getElementById('primary-view'));
-        }
-    },
-
-    _login: function() {
-        if (this.app.config.auth && !this.app.state.user) {
-            ReactDOM.render(<LoginView
-                router={this}
-                dispatcher={this.app}
-                twoFactor={this.app.config.two_factor}
-            />, document.getElementById('primary-view'));
-        } else {
-            // No login needed, go to Browse
-            this.navigate('browse', { trigger: true });
-        }
-    },
-
-    _register: function() {
-        if (this.app.config.auth && !this.app.state.user) {
-            ReactDOM.render(<RegisterView
-                router={this}
-            />, document.getElementById('primary-view'));
-        } else {
-            // No login needed, go to Browse
-            this.navigate('browse', { trigger: true });
-        }
-    },
-
     _model: function(modelID, query) {
-        if (this.app.config.auth && !this.app.state.user) {
-            // Redirect to Login if auth required and not done
-            this.navigate('login', { trigger: true });
-        } else {
-            query = queryString.parse(query);
-            let self = this;
-            // Render the root CAD view
-            ReactDOM.render(<CADView
-                manager={this.app.cadManager}
-                viewContainerId='primary-view'
-                root3DObject={this.app._root3DObject}
-            />, document.getElementById('primary-view'), function () {
-                // Dispatch setModel to the CADManager
-                self.app.cadManager.dispatchEvent({
-                    type: 'setModel',
-                    path: modelID,
-                    baseURL: self.app.services.api_endpoint + self.app.services.version,
-                    modelType: query.type
-                });
+        query = queryString.parse(query);
+        let self = this;
+        // Render the root CAD view
+        ReactDOM.render(<CADView
+            manager={this.app.cadManager}
+            viewContainerId='primary-view'
+            root3DObject={this.app._root3DObject}
+        />, document.getElementById('primary-view'), function () {
+            // Dispatch setModel to the CADManager
+            self.app.cadManager.dispatchEvent({
+                type: 'setModel',
+                path: modelID,
+                baseURL: self.app.services.api_endpoint + self.app.services.version,
+                modelType: query.type
             });
-        }
+        });
     },
 
     _stepnc: function(pid){
         let self = this;
         
-        
-
         let url = "/v2/nc/projects/";
         
         let requestCB = function(error, response) {
@@ -143,7 +89,6 @@ module.exports = Backbone.Router.extend({
         requestCB = requestCB.bind(this);
         
         request.get(url).end(requestCB);
-
 
     },
 
