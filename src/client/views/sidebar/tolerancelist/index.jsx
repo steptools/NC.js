4cebcdc97ee39bdfc51a7b5a68ad27482e294e6d
Ian Chamberlain <ian.h.chamberlain@gmail.com>
2016-06-24 10:05:16 -0400

    Added icons for tolerance based on type

--- src/client/views/sidebar/tolerancelist/index.jsx
+++ src/client/views/sidebar/tolerancelist/index.jsx
@@ -35,9 +35,15 @@ export default class ToleranceList extends React.Component {
       let url = "/v2/nc/projects/"+this.props.pid+"/tolerances/";
       let resCb = function(err,res){ //Callback function for response
             if(!err && res.ok){
-              // Node preprocessing
-              let json = JSON.parse(res.text);
-              this.setState({tolerances: json});
+                // Node preprocessing
+                let json = JSON.parse(res.text);
+
+                _.each(json, (tolerance) => {
+                    tolerance.type = tolerance.type.replace(/_TOLERANCE/g, '').toLowerCase();
+                    tolerance.icon = <span className={'icon-tolerance tolerance-'+tolerance.type} />
+                })
+
+                this.setState({tolerances: json});
             }
       }
       resCb = resCb.bind(this); //Needs to bind this in order to have correct this
