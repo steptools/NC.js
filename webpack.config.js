f5a87b9dbcfc2a3b55a0e55563ed8ec9ea801b20
SjBeale <beales@rpi.edu>
2016-06-28 15:21:03 -0400

    Removed commented out code

--- webpack.config.js
+++ webpack.config.js
@@ -57,34 +57,7 @@ module.exports = {
             underscore  : "lodash"
         }
     },
-    plugins: [/*
-            new webpack.DefinePlugin({
-                'process.env': {
-            
-                'NODE_ENV': JSON.stringify('production'),
-            }
-            }),
-            new webpack.optimize.DedupePlugin(),
-            new webpack.optimize.UglifyJsPlugin({
-                sourceMap: false,
-                compress: {
-                    sequences: true,
-                    dead_code: true,
-                    conditionals: true,
-                    booleans: true,
-                    unused: true,
-                    if_return: true,
-                    join_vars: true,
-                    drop_console: true
-                },
-                mangle: {
-                    except: ['$super', '$', 'exports', 'require']
-                },
-                output: {
-                    comments: false
-                } 
-            }),
-        */
+    plugins: [
         new webpack.ProvidePlugin({
             "_":        "lodash",
             "$":        "jquery",
