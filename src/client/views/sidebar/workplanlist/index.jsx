fa86d213962fbe0246b55e65cc08e38290841950
Ian Chamberlain <ian.h.chamberlain@gmail.com>
2016-06-30 09:37:15 -0400

    Makes workplan view non-draggable

--- src/client/views/sidebar/workplanlist/index.jsx
+++ src/client/views/sidebar/workplanlist/index.jsx
@@ -30,6 +30,7 @@ export default class WorkplanList extends React.Component {
           id={node.id}
           className={cName}
           onClick={(event) => {this.props.propertyCb(node);}}
+          onMouseDown={function(e){e.stopPropagation()}}
       >
           {node.icon}
           {node.name}
