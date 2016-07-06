26b35a4a7fff73defa77a55743dceaa0c6f6e898
Patrick Hesselbach <hessep@rpi.edu>
2016-07-06 14:35:14 -0400

    too many }

--- src/client/views/sidebar/workplanlist/index.jsx
+++ src/client/views/sidebar/workplanlist/index.jsx
@@ -31,12 +31,12 @@ export default class WorkplanList extends React.Component {
         }
         return this.props.ws !== nextProps.ws;
     }
-    
+
     toggleToCurrentWS(node) {
         if (node.id === this.props.ws) {
             node.toggled = true;
             return true;
-        } 
+        }
         if (node.enabled && !node.leaf) {
             for (let i = 0; i < node.children.length; i++) {
                 if (this.toggleToCurrentWS(node.children[i])) {
@@ -47,10 +47,8 @@ export default class WorkplanList extends React.Component {
         }
         node.toggled = false;
     }
-  }
 
     render() {
-        //console.log("Rendering workplan view");
         this.decorators.ws = this.props.ws;
         return (
             <Treebeard data={this.props.workplanCache} onToggle={this.onToggle} style={ts.style} decorators={this.decorators}/>
