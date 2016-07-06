79c9cc62dda874cff37b69e7214040464db05ae6
nicholasfay <fayn@rpi.edu>
2016-07-06 16:13:55 -0400

    fixed

--- src/server/api/v3/file.js
+++ src/server/api/v3/file.js
@@ -11,22 +11,21 @@ let tol;
 let ms;
 
 function init(path, machinetool){
+	fs.accessSync(path, fs.R_OK , (err) => {
+  		process.exit();
+	});
 	this.find = new StepNC.Finder();
 	this.apt = new StepNC.AptStepMaker();
 	this.tol = new StepNC.Tolerance();
 	this.ms = new StepNC.machineState(path);
-	if(!this.ms)
-		process.exit();
 	if(machinetool !== ""){
 		if(!this.ms.LoadMachine(machinetool))
 			console.log("ERROR: Machinetool was not loaded");
 		else
 			console.log("Loaded Machine Successfully")
 	}
-	if(!this.find.OpenProject(path))
-		process.exit();
-	if(!this.apt.OpenProject(path))
-		process.exit();
+	this.find.OpenProject(path);
+	this.apt.OpenProject(path);
 	return;
 }
 
