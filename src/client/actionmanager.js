a09d17c3d56c5ba45e0914c75ff83905392ebb3a
Will Rigby-Hall <will@wgrh.io>
2016-07-25 10:30:35 -0400

    Styleguide changes to actionmanager and main

--- src/client/actionmanager.js
+++ src/client/actionmanager.js
@@ -6,7 +6,7 @@
 
 let LOG_ALL_ACTIONS = false;
 
-if (LOG_ALL_ACTIONS){
+if (LOG_ALL_ACTIONS) {
   console.info(`
     The action manager is currently set to log all messages.
 
@@ -18,21 +18,22 @@ if (LOG_ALL_ACTIONS){
 const EventEmitter = require('events');
 
 class ActionManager extends EventEmitter {
-  constructor(){
+  constructor() {
     super();
-    this.on("newListener", (event)=>{
-      if (LOG_ALL_ACTIONS)
-        console.log("ActionManager : New Listener : " + event);
+    this.on('newListener', (event)=>{
+      if (LOG_ALL_ACTIONS) {
+        console.log('ActionManager : New Listener : ' + event);
+      }
     });
   }
 }
 
 const actionManager = new ActionManager();
 
-if (LOG_ALL_ACTIONS){
+if (LOG_ALL_ACTIONS) {
   let oldEmit = actionManager.emit;
-  actionManager.emit = function(event, data){
-    console.log("ActionManager : Action : " + event);
+  actionManager.emit = function(event) {
+    console.log('ActionManager : Action : ' + event);
     oldEmit.apply(actionManager, arguments);
   };
 }
--- src/client/main.js
+++ src/client/main.js
@@ -1,94 +1,94 @@
 /* Copyright G. Hemingway, 2015 - All rights reserved */
-"use strict";
+'use strict';
 
 // Necessary modules
 require('./stylesheets/base.scss');
 require('bootstrap-webpack');
-let io              = require('socket.io-client'),
-    jwtDecode       = require('jwt-decode'),
-    actionManager   = require('./actionmanager');
+let io = require('socket.io-client');
+let jwtDecode = require('jwt-decode');
+let actionManager = require('./actionmanager');
 import CADManager from './models/cad_manager';
-import React                from 'react';
-import ReactDOM             from 'react-dom';
-import ResponsiveView       from './views/responsive';
-
-/*************************************************************************/
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ResponsiveView from './views/responsive';
 
 class CADApp extends THREE.EventDispatcher {
-    constructor() {
-        super();
-        let $body = $('body');
-        this.services = $body.data('services');
-        this.config = $body.data('config');
-        // Set state
-        this.state = {
-            token: window.localStorage.getItem('user:token'),
-            user: undefined
-        };
-        if (this.state.token) {
-            this.handleLogin({ token: this.state.token });
-        } else {
-            this.addEventListener('user:login', this.handleLogin.bind(this));
-        }
-
-        // Setup socket
-        this.socket = undefined;
-        if (this.config.socket) {
-            // Establish socket connection
-            let socketURL = this.services.api_endpoint + this.services.socket;
-            this.socket = io(socketURL, {});
-            // Connect to the socket server
-            this.socket.on('connect', function () {
-                console.log('Socket client connected');
-            });
-        }
-        // Create data manager
-        this.cadManager = new CADManager(this.config, this.socket,this);
-
-        // Create application-level action manager
-        this.actionManager = actionManager;
-
-        // Initialize views
-        $body.toggleClass('non-initialized');
-        // Initialize the views and dispatch the event to set the model
-        ReactDOM.render(
-            <div style={{ height:'100%'}}>
-                <ResponsiveView
-                    app = {this}
-                    />
-            </div>
-            , document.getElementById('primary-view'), function () {
-                // Dispatch setModel to the CADManager
-        });
-
-        this.cadManager.dispatchEvent({
-            type: 'setModel',
-            path: 'state/key',
-            baseURL: this.services.api_endpoint + this.services.version,
-            modelType: 'nc'
-        });
+  constructor() {
+    super();
+    let $body = $('body');
+    this.services = $body.data('services');
+    this.config = $body.data('config');
+    // Set state
+    this.state = {
+      token: window.localStorage.getItem('user:token'),
+      user: undefined,
+    };
+    if (this.state.token) {
+      this.handleLogin({token: this.state.token});
+    } else {
+      this.addEventListener('user:login', this.handleLogin.bind(this));
     }
 
-    handleLogin(ev) {
-        // Set app state
-        window.localStorage.setItem('user:token', ev.token);
-        this.state.token = ev.token;
-        this.state.user = jwtDecode(this.state.token);
-        this.removeEventListener('user:login', this.handleLogin.bind(this));
-        this.addEventListener('user:logout', this.handleLogout.bind(this));
+    // Setup socket
+    this.socket = undefined;
+    if (this.config.socket) {
+      // Establish socket connection
+      let socketURL = this.services.api_endpoint + this.services.socket;
+      this.socket = io(socketURL, {});
+      // Connect to the socket server
+      this.socket.on('connect', function () {
+        console.log('Socket client connected');
+      });
     }
+    // Create data manager
+    this.cadManager = new CADManager(this.config, this.socket,this);
 
-    handleLogout() {
-        // Update logged out state
-        window.localStorage.setItem('user:token', undefined);
-        this.state.token = undefined;
-        this.state.user = undefined;
-        this.removeEventListener('user:logout', this.handleLogout.bind(this));
-        this.addEventListener('user:login', this.handleLogin.bind(this));
-    }
+    // Create application-level action manager
+    this.actionManager = actionManager;
+
+    // Initialize views
+    $body.toggleClass('non-initialized');
+    // Initialize the views and dispatch the event to set the model
+    let view = (
+      <div style={{height:'100%'}}>
+        <ResponsiveView
+          app = {this}
+        />
+      </div>
+    );
+    ReactDOM.render(
+      view,
+      document.getElementById('primary-view'), function () {
+        // Dispatch setModel to the CADManager
+      }
+    );
+
+    this.cadManager.dispatchEvent({
+      type: 'setModel',
+      path: 'state/key',
+      baseURL: this.services.api_endpoint + this.services.version,
+      modelType: 'nc',
+    });
+  }
+
+  handleLogin(ev) {
+    // Set app state
+    window.localStorage.setItem('user:token', ev.token);
+    this.state.token = ev.token;
+    this.state.user = jwtDecode(this.state.token);
+    this.removeEventListener('user:login', this.handleLogin.bind(this));
+    this.addEventListener('user:logout', this.handleLogout.bind(this));
+  }
+
+  handleLogout() {
+    // Update logged out state
+    window.localStorage.setItem('user:token', undefined);
+    this.state.token = undefined;
+    this.state.user = undefined;
+    this.removeEventListener('user:logout', this.handleLogout.bind(this));
+    this.addEventListener('user:login', this.handleLogin.bind(this));
+  }
 }
-
-/*************************************************************************/
 
 // Invoke the new app
 module.exports = new CADApp();
