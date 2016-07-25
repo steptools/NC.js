3e3d2f8d309db9e4cd91f25bca96856fe454975e
Will Rigby-Hall <will@wgrh.io>
2016-07-25 11:42:44 -0400

    Changelog.js styleguide changes

--- src/server/api/v3/changelog.js
+++ src/server/api/v3/changelog.js
@@ -1,34 +1,20 @@
-"use strict";
-var StepNC = require('../../../../../STEPNode/build/Release/StepNode');
+'use strict';
 var fs = require('fs');
 
-///*******************************************************************\
-//|                                                                    |
-//|                       Helper Functions                             |
-//|                                                                    |
-//\*******************************************************************/
+/***************************** Endpoint Functions *****************************/
 
-
-
-
-///*******************************************************************\
-//|                                                                    |
-//|                       Endpoint Functions                           |
-//|                                                                    |
-//\*******************************************************************/
-
-var _getchangelog = function(req,res){
-    fs.readFile("changelog.md", "utf8", function(err,data){
-      if(err) {
-        return console.log(err);
-      }
-      res.status(200).send(data);
-    })
-
-
-}
+function _getchangelog(req, res) {
+  fs.readFile('changelog.md', 'utf8', function(err, data) {
+    if (err) {
+      return console.log(err);
+    }
+    res.status(200).send(data);
+  });
+};
 
 module.exports = function(app, cb) {
   app.router.get('/log', _getchangelog);
-  if (cb) cb();
+  if (cb) {
+    cb();
+  }
 };
