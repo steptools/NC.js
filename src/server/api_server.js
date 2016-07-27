399f33f1f78e2096ed585227f3bb38671e34b4a2
nicholasfay <fayn@rpi.edu>
2016-07-27 11:22:24 -0400

    style guide

--- src/server/api_server.js
+++ src/server/api_server.js
@@ -107,13 +107,13 @@ APIServer.prototype._setSite = function() {
   var endpoint = this.config.host ? this.config.protocol + '://' + this.config.host + ':' + app.port : '';
   var services = {
     apiEndpoint: endpoint,
-    socket: "",
+    socket: '',
     version: '/v3',
     machine: self.machinetool,
   };
   // Serve the root client framework - customized as needed
   var _serveRoot = function (req, res) {
-    var change = fs.readFileSync("CHANGELOG.md", "utf8");
+    var change = fs.readFileSync('CHANGELOG.md', 'utf8');
     var appConfig = {
       title: 'NC.js',
       source: '/js/main.js',
