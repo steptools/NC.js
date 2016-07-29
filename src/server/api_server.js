79ad82e2b41475d901e67d9ac289295e38e0d30b
Patrick Hesselbach <hessep@rpi.edu>
2016-07-29 13:22:25 -0400

    Works!

--- src/server/api_server.js
+++ src/server/api_server.js
@@ -12,7 +12,7 @@ function createWindow () {
   win = new BrowserWindow({width: 800, height: 600});
 
   // and load the index.html of the app.
-  win.loadURL(`localhost:8080`);
+  win.loadURL(`http://localhost:8080`);
 
   // Open the DevTools.
   win.webContents.openDevTools();
