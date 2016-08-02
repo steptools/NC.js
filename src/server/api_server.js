a3d2467747a4bf2f3a002b20d7bb49f2e1f2bbb7
SjBeale <beales@rpi.edu>
2016-08-02 16:25:47 -0400

    HIde devtools and make screen default  max size

--- src/server/api_server.js
+++ src/server/api_server.js
@@ -9,13 +9,10 @@ let win;
 function createWindow () {
   // Create the browser window.
   win = new BrowserWindow({width: 800, height: 600});
-
+  win.maximize();
   // and load the index.html of the app.
   win.loadURL(`http://localhost:8080`);
-
-  // Open the DevTools.
-  win.webContents.openDevTools();
-
+  
   // Emitted when the window is closed.
   win.on('closed', () => {
     // Dereference the window object, usually you would store windows
