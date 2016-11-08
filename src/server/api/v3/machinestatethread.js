e5f769c4d2edec40e5ca9cfc80a645052e099f3f
Samson <bonfante@steptools.com>
2016-11-08 10:55:00 -0500

    Refactored code to use node fork instead of the incredibly crummy tiny-worker package. Also switched from using a single in/out pipe to a dedicated pipe for each- should prevent issues related to pipe saturation.

--- src/server/api/v3/machinestatethread.js
+++ src/server/api/v3/machinestatethread.js
@@ -1,29 +1,39 @@
 'use strict';
 let StepNC = require('STEPNode');
+let readline = require('readline');
 let ms = {};
-onmessage = (f)=>{
-  f=f.data;
-  if(f.msg==='setMachineState') {
-    ms = new StepNC.machineState(f.path, f.sim);
-    if (f.hasOwnProperty('tool'))
-      ms.LoadMachine(f.tool);
-  } else if(f.msg==='getMachine') {
-    let out;
-    if(!f.hasOwnProperty('args') || Object.keys(f.args).length <0) {
+let parseMessage =
+  (f)=>{
+    if(f.msg==='setMachineState') {
+      ms = new StepNC.machineState(f.path, f.sim);
+      if (f.hasOwnProperty('tool'))
+        ms.LoadMachine(f.tool);
+    } else if(f.msg==='getMachine') {
+      let out;
+      if(!f.hasOwnProperty('args') || Object.keys(f.args).length <0) {
         Promise.resolve(ms[f.fun]()).then((r)=> {
-          postMessage({'cb': f.callback, 'val': r});
+          process.send({'cb': f.callback, 'val': r});
         });
-        return;
-    } else {
-      if(!f.args.hasOwnProperty('length')){
-        f.args.length = Object.keys(f.args).length;
-      }
-      //Node Native Module calls need to invoke apply with the relevant object.
-      //This next line is hairy, but it basically invokes the function our caller wants with the arguments array we have
+      } else {
+        if(!f.args.hasOwnProperty('length')){
+          f.args.length = Object.keys(f.args).length;
+        }
+        //Node Native Module calls need to invoke apply with the relevant object.
+        //This next line is hairy, but it basically invokes the function our caller wants with the arguments array we have
         Promise.resolve(ms[f.fun].apply(ms,Array.prototype.slice.call(f.args)))
           .then((r)=>{
-            postMessage({'cb': f.callback, 'val': r});
+            process.send({'cb': f.callback, 'val': r});
           });
+      }
     }
-  }
-};
+  };
+
+process.on('message',parseMessage);
+
+let rl = readline.createInterface({input:process.stdin,output:process.stdout});
+rl.on('line',(input)=>{
+  let i = JSON.parse(input);
+  parseMessage(i);
+});
+
+console.log('MachineState worker started PID %s\r\n',process.pid);
