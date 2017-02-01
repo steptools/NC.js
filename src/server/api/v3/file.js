aaf5437efa976ac320fe8a5a0d70affea09360fd
Samson <bonfante@steptools.com>
2017-02-01 11:49:46 -0500

    refactored file.js to match styleguide and hopefully be a little clearer.

--- src/server/api/v3/file.js
+++ src/server/api/v3/file.js
@@ -9,7 +9,7 @@ worker.stderr.pipe(process.stderr);
 let Path = require('path');
 let resolves = {};
 
-let project = "";
+let project = '';
 
 
 let waittosend=false;
@@ -17,56 +17,71 @@ worker.on('message',(ev)=>{
   let cb = eval(ev.cb);
   cb(ev.val);
 });
-worker.on('error',(err)=>{console.log(err);});
+worker.on('error',(err)=>{
+  console.log(err);
+});
 let drainqueue=[];
 let runningdrainqueue=false;
 let emptydrainqueue = ()=>{
-  if(runningdrainqueue) return;
+  if (runningdrainqueue) {
+    return;
+  }
   runningdrainqueue = true;
-  while(!_.isEmpty(drainqueue) && !waittosend){
+  while (!_.isEmpty(drainqueue) && !waittosend) {
     let msg = drainqueue.shift();
     let shouldntwait = worker.stdin.write(JSON.stringify(msg) + '\r\n');
     waittosend = !shouldntwait;
   }
   runningdrainqueue=false;
 };
-worker.stdin.on('drain',()=>{waittosend=false; emptydrainqueue();});
+worker.stdin.on('drain',()=>{
+  waittosend=false;
+  emptydrainqueue();
+});
 
 let msid = 0;
 
-function init(path, machinetool) {
-  fs.accessSync(path, fs.R_OK , () => process.exit());
-
-  this.project = Path.basename(path,Path.extname(path));
+function openSimpleNcs(path){
   this.apt = new StepNC.AptStepMaker();
-  try {
-    fs.accessSync(machinetool,fs.R_OK)
-  } catch (e){
-    machinetool="";
-  }
   this.find = new StepNC.Finder();
-  this.tol = new StepNC.Tolerance();
 
   this.apt.OpenProject(path);
   this.find.OpenProject(path);
-  let setms ={ 'msg':'setMachineState','path':path,'sim':true};
-  if(machinetool!=="") setms.tool = machinetool;
-	worker.stdin.write(JSON.stringify(setms)+'\r\n');
-  this.ms = {};
-  //The following function maps STEPNode.machineState functions into worker-thread message calls.
-  //It's kinda like black magic.
-  _.forIn(StepNC.machineState.prototype,(val,key)=>{
 
+  this.tol = new StepNC.Tolerance(); //tol keys off of apt.
+}
+function openMachineState(path,tool){
+  let setms ={'msg':'setMachineState','path':path,'sim':true};
+  if (tool!=='') {
+    setms.tool = tool;
+  }
+  worker.stdin.write(JSON.stringify(setms)+'\r\n');
+  this.ms = {};
+  //Makes a function for the worker that looks like
+  //(data)=>{
+  //  resolves[12](data);
+  //  resolves[12]=null;
+  //  data=null;
+  //};
+  let workerstring = (id)=>{
+    return '(data)=>{resolves['+id+
+      '](data);resolves['+id+']=null;data=null;};';
+  };
+  //Map STEPNode.machineState functions to worker-thread message calls.
+  //It's kinda like black magic.
+  //Should be deprecated by actual promise-based implementation of STEPNode.
+  _.forIn(StepNC.machineState.prototype,(val,key)=>{
     this.ms[key] = function(){
       return new Promise((resolve)=>{
-        resolves[msid] = (d)=>{resolve(d)};
-        let mas="(data)=>{resolves["+msid+"](data);resolves["+msid+"]=null;data=null;};";
+        resolves[msid] = d=>resolve(d);
+        let mas=workerstring(msid);
         let msg = {'msg':'getMachine','fun':key,'callback':mas};
-        if(arguments.length>0) msg['args']=arguments;
-        if(waittosend ===true) {
-          drainqueue.push(msg);
+        if (arguments.length>0) {
+          msg['args']=arguments;
         }
-        else {
+        if (waittosend ===true) {
+          drainqueue.push(msg);
+        } else {
           let shouldntwait = worker.stdin.write(JSON.stringify(msg) + '\r\n');
           waittosend = !shouldntwait;
         }
@@ -74,7 +89,18 @@ function init(path, machinetool) {
       });
     };
   });
+}
+function init(path, machinetool) {
+  fs.accessSync(path, fs.R_OK , () => process.exit());
 
+  this.project = Path.basename(path,Path.extname(path));
+  try {
+    fs.accessSync(machinetool,fs.R_OK);
+  } catch (e){
+    machinetool='';
+  }
+  openSimpleNcs(path);
+  openMachineState(path,machinetool);
 	return;
 }
 
