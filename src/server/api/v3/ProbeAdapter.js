64055e121979e6978b866142397da9ce61451087
Samson <samson@bonfante.org>
2017-02-09 12:11:38 -0500

    Program info added to adapter

--- src/server/api/v3/ProbeAdapter.js
+++ src/server/api/v3/ProbeAdapter.js
@@ -3,6 +3,7 @@ let net = require('net');
 let server = net.createServer((socket)=>{
 });
 let ports = [];
+let program ="";
 let ping = (data,socket)=>{
     socket.write("* PONG 10000");
 };
@@ -13,13 +14,19 @@ server.on('connection',(socket)=>{
     socket.on('data',(r)=>{
         ping(r,socket);
     });
+    socket.write(new Date(Date.now()).toISOString()+"|program|"+program);
     ports.push(socket);
 });
 
 let sendClients = (msg)=>{
     ports.forEach((client)=>{
-        client.write(msg);
+        client.write(msg,()=>{console.log("wrote %j to %j",client,msg)});
     });
 };
 
+let setID = (name)=>{
+    program=name;
+    sendClients(new Date(Date.now()).toISOString()+"|program|"+program);
+};
 module.exports.write = sendClients;
+module.exports.ProgramID = setID;
