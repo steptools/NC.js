ba1ba05186aadc2c782f8e79240b6997a4b74423
Samson <bonfante@steptools.com>
2017-07-25 15:35:29 -0400

    Annotations have new format.

--- src/client/models/annotation.js
+++ src/client/models/annotation.js
@@ -5,85 +5,62 @@
 /********************************* Annotation Class ********************************/
 
 export default class Annotation extends THREE.EventDispatcher {
-    constructor(id, model,isLive) {
-        super();
-        this._id = id;
-        this._model = model;
-        this._geometry = undefined;
-        this.live = isLive;
-        this.visible = true;
-        return this;
-        this.toggleScene = this.toggleScene.bind(this);
+  constructor(id, model, isLive) {
+    super();
+    this._id = id;
+    this._model = model;
+    this._geometry = undefined;
+    this._lines = [];
+    this.live = isLive;
+    this.visible = true;
+    return this;
+    this.toggleScene = this.toggleScene.bind(this);
+  }
+
+  getID() {
+    return this._id;
+  }
+
+  addGeometry(data) {
+    for (const polyline of data) {
+      let geometry = new THREE.BufferGeometry();
+      //load points & colors
+      let vertices = new Float32Array(polyline.points.length * 3);
+      let colorArray = new Float32Array(polyline.points.length * 3);
+      _.forEach(polyline.points, (point, index) => {
+        let bufferIndex = index*3;
+        vertices[bufferIndex]     = point[0];            // X
+        vertices[bufferIndex + 1] = point[1];            // Y
+        vertices[bufferIndex + 2] = point[2];            // Z
+        colorArray[bufferIndex]     = polyline.color[0]; // R
+        colorArray[bufferIndex + 1] = polyline.color[1]; // G
+        colorArray[bufferIndex + 2] = polyline.color[2]; // B
+      });
+      let position = new THREE.BufferAttribute(vertices, 3);
+      geometry.addAttribute('position', position);
+      let color = new THREE.BufferAttribute(colorArray, 3);
+      geometry.addAttribute('color', color);
+      this._lines.push(geometry);
     }
+    // All done - signal completion
+    this.dispatchEvent({ type: "annotationEndLoad", annotation: this });
+  }
 
-    getID() {
-        return  this._id;
-    }
+  toggleScene() {
+    if (this.visible) this.removeFromScene();
+    else this.addToScene();
+  }
 
-    addGeometry(data) {
-        let decompressColors = function(lData, cData, defaultColor=[1.0, 0.0, 0.0]) {
-            var ar = _.map(lData, function(lineStrip, stripIndex) {
-                let numVertices = lineStrip.length / 3;
-                let colors = new Float32Array(lineStrip.length);
-                let color = defaultColor;
-                // If there is color data, process it
-                if (cData) {
-                    let index = 0;
-                    for (let i = 0; i < cData.length; i++) {
-                        index += cData[i].duration;
-                        //if (stripIndex <= index) {
-                        //    color = cData[i].data;
-                        //}
-                        color = cData[i].data;
-                        if (index > stripIndex) break;
-                    }
-                }
-                // Build colors array
-                for (var i=0; i < numVertices; i++) {
-                    colors[i * 3] = color[0];
-                    colors[i * 3 + 1] = color[1];
-                    colors[i * 3 + 2] = color[2];
-                }
+  addToScene() {
+    this.dispatchEvent({ type: "annotationMakeVisible", annotation: this });
+    this.visible = true;
+  }
+  removeFromScene() {
+    this.dispatchEvent({ type: "annotationMakeNonVisible", annotation: this });
+    this.visible = false;
+  }
 
-                return colors;
-            });
-            return ar;
-        };
-        // Decompress the colors data
-        let colorArrays = decompressColors(data.lines, data.colorsData);
-
-        // Create geometry for each linestrip
-        this._lines =_.map(data.lines, function(lineStrip, index) {
-            let geometry = new THREE.BufferGeometry();
-
-            let vertices = new Float32Array(lineStrip.length);
-            for (let i=0; i<lineStrip.length; i++) vertices[i]=lineStrip[i];
-            let position = new THREE.BufferAttribute(vertices, 3);
-            geometry.addAttribute('position', position );
-
-            let colors = new THREE.BufferAttribute(colorArrays[index], 3);
-            geometry.addAttribute('color', colors);
-            return geometry;
-        });
-        // All done - signal completion
-        this.dispatchEvent({ type: "annotationEndLoad", annotation: this });
-    }
-
-    toggleScene(){
-        if(this.visible) this.removeFromScene();
-        else this.addToScene();
-    }
-
-    addToScene(){
-      this.dispatchEvent({ type: "annotationMakeVisible", annotation: this });
-        this.visible=true;
-    }
-    removeFromScene(){
-      this.dispatchEvent({ type: "annotationMakeNonVisible", annotation: this });
-        this.visible=false;
-    }
-
-    getGeometry() {
-        return this._lines;
-    }
+  getGeometry() {
+    return this._lines;
+  }
 };
